f8cf202ba596317593429d1c8c5caf2521dd33ef
Drew Immerman <drewimmerman@gmail.com>
2023-04-08 17:49:00 -0400

    Add support for creating a table if not exists

--- src/ddl-builder.ts
+++ src/ddl-builder.ts
@@ -1,7 +1,25 @@
-import { ColumnOptions, DataDefinitionBuilder } from 'riao-dbal/src';
+import { CreateTableOptions, DataDefinitionBuilder } from 'riao-dbal/src';
 
 export class MsSqlDataDefinitionBuilder extends DataDefinitionBuilder {
 	public getAutoIncrement(): string {
 		return 'IDENTITY';
+	}
+
+	public ifNotExists(): this {
+		return this;
+	}
+
+	public createTable(options: CreateTableOptions): this {
+		if (options.ifNotExists) {
+			this.sql += `IF OBJECT_ID(N'dbo.${options.name}', N'U') IS NULL BEGIN `;
+		}
+
+		super.createTable(options);
+
+		if (options.ifNotExists) {
+			this.sql += ' END';
+		}
+
+		return this;
 	}
 }
