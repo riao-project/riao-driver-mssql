f22f77a5f30b53f4ebe8d8d84861eeb826279de6
Drew Immerman <drewimmerman@gmail.com>
2024-07-07 18:49:32 -0400

    dbal-125 Add support for offset in SelectQuery

--- src/query-builder.ts
+++ src/query-builder.ts
@@ -1,9 +1,18 @@
-import { DatabaseQueryBuilder } from '@riao/dbal';
+import { DatabaseQueryBuilder, DatabaseRecord, SelectQuery } from '@riao/dbal';
 import { MsSqlBuilder } from './sql-builder';
 
 export class MsSqlQueryBuilder extends DatabaseQueryBuilder {
 	public getSqlType() {
 		return MsSqlBuilder;
+	}
+
+	public select(query: SelectQuery<DatabaseRecord>): this {
+		if (query.limit && !query.offset) {
+			query['top'] = query.limit;
+			delete query.limit;
+		}
+
+		return super.select(query);
 	}
 
 	public insertOutput(primaryKey: string): this {
@@ -18,7 +27,35 @@ export class MsSqlQueryBuilder extends DatabaseQueryBuilder {
 		return this;
 	}
 
+	public pagination(query: SelectQuery): this {
+		if (query.offset !== undefined) {
+			if (query.orderBy === undefined) {
+				throw new Error('Cannot offset without order by!');
+			}
+
+			if (!query.limit === undefined) {
+				throw new Error('Cannot offset without limit!');
+			}
+
+			this.offset(query.offset);
+		}
+
+		if (query.limit) {
+			this.limit(query.limit);
+		}
+
+		return this;
+	}
+
+	public offset(nRecords: number): this {
+		this.sql.append('OFFSET ' + nRecords + ' ROWS ');
+
+		return this;
+	}
+
 	public limit(nRecords: number): this {
+		this.sql.append('FETCH NEXT ' + nRecords + ' ROWS ONLY ');
+
 		return this;
 	}
 
